2adccdd392baca2dc23ced00a47d429d1077ceaf
Hills007 <toorkhan2000@yahoo.com>
2024-03-25 10:04:31 +0000

    define display methon and implement it

--- run.py
+++ run.py
@@ -2,10 +2,14 @@ from random import randint
 scores = {"computer": 0, "player": 0}
 class Board:
     def __init__(self, size, num_ships, name, board_type):
-    self.size = size
-    self.board = [["." for _ in range(size)] for _ in range(size)]
-    self.num_ships = num_ships
-    self.name = name
-    self.type = board_type
-    self.guesses = []
-    self.ships = []
+        self.size = size
+        self.board = [["." for _ in range(size)] for _ in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = board_type
+        self.guesses = []
+        self.ships = []
+        
+     def display(self):
+        for row in self.board:
+            print(" ".join(row))       
