7b9f2826f40588f5fefa1685107360941d13e5c2
Hills007 <151192052+Hills007@users.noreply.github.com>
2024-04-08 13:47:36 +0100

    Update run.py
    Remove the populate board function for player to fix error when starting game

--- run.py
+++ run.py
@@ -102,9 +102,8 @@ def new_game():
     computer_board = Board(size, num_ships, "Computer", board_type="computer")
     player_board = Board(size, num_ships, player_name, board_type="player")
 
-    for _ in range(num_ships):
-        populate_board(player_board)
-        populate_board(computer_board)
+
+    populate_board(computer_board)
 
     play_game(computer_board, player_board)
 
